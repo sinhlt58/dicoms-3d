039c7994a7ae558b8a9bfbb6cec1dd3bbf6d830e
Luu Truong Sinh <sinhlt58@gmail.com>
2022-05-31 00:03:27 +0700

    update read multiple file name

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import vtkImageData from '@kitware/vtk.js/Common/DataModel/ImageData';
-import { readImageDICOMFileSeries } from 'itk-wasm';
+import { readImageArrayBuffer, readImageDICOMFileSeries } from 'itk-wasm';
 import { useState } from 'react';
 import './App.css';
 import { helper } from './helper';
 import { ThreeDEditorProvider } from './components/threeD-editor.provider';
+import vtkITKImageReader from '@kitware/vtk.js/IO/Misc/ITKImageReader';
+
+vtkITKImageReader.setReadImageArrayBufferFromITK(readImageArrayBuffer);
 
 function App() {
 
@@ -20,13 +23,27 @@ function App() {
     e.target.value = null; // no need to keep this in the memory
   }
 
+  const handleFileNifti = async (e: any) => {
+    const file: File = e.target.files[0];
+    const arrayBuffer = await file.arrayBuffer();
+    const {image: itkImage} = await readImageArrayBuffer(null, arrayBuffer, file.name, "");
+    const vtkImage: vtkImageData = helper.convertItkToVtkImage(itkImage) as vtkImageData;
+    setVtkImage(vtkImage);
+    e.target.value = null; // no need to keep this in the memory
+  }
+
   return (
     <div className="flex flex-col h-full">
-      <div className="flex p-4 border-b-2 border-gray-200 bg-gray-100">
+      <div className="flex gap-2 p-4 border-b-2 border-gray-200 bg-gray-100">
         <input
           type="file"
           onChange={handleFileChanged}
           multiple
+          onClick={(e: any) => e.target.value = null}
+        />
+        <input
+          type="file"
+          onChange={handleFileNifti}
           onClick={(e: any) => e.target.value = null}
         />
       </div>
