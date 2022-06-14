c42bce093bc0a915c4144957cc9c90a1ae1c5770
Luu Truong Sinh <sinhlt2@vingroup.net>
2022-06-14 17:21:02 +0700

    update

--- scripts/dicoms2niffty.py
+++ scripts/dicoms2niffty.py
@@ -4,17 +4,20 @@ import nibabel as nib
 
 
 # ds = dcmread("./examples/breast/1.dcm")
-ds = dcmread("./examples/PAT033/D0001.dcm")
-# print(ds.file_meta)
-# print(ds[0x0018, 0x5101].value)
-# print(ds[0x0020, 0x0060].value)
+# ds = dcmread("./examples/PAT033/D0001.dcm")
+ds = dcmread("./examples/3.DCM")
+# Breast
+print(ds.file_meta)
+print(ds[0x0018, 0x5101].value)
+print(ds[0x0020, 0x0062].value)
+print(ds[0x0020, 0x0060].value)
 
 print("Pixel per row: ", ds[0x0028, 0x0010])
 print("Pixel per column: ", ds[0x0028, 0x0011])
 # print("Space bewteen slice: ", ds[0x0018, 0x0088])
 
-# print("PixelSpacing: ", ds[0x0028, 0x0030].value)
-# print("SliceThickness: ", ds[0x0018, 0x0050].value)
+print("PixelSpacing: ", ds[0x0028, 0x0030].value)
+print("SliceThickness: ", ds[0x0018, 0x0050].value)
 # print("SpacingBetweenSlices: ", ds[0x0018, 0x0088].value)
 
 # dicom2nifti.dicom_series_to_nifti(
@@ -24,6 +27,9 @@ print("Pixel per column: ", ds[0x0028, 0x0011])
 # )
 
 img = nib.load("./examples/PAT033.nii")
+print("========================")
 print(img.shape)
 print(img.get_data_dtype())
 print(img.get_fdata().shape)
+
+print (img.get_fdata())
